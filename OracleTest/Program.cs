006694a99f21a0a90eba4086e3675c6a7262bc8f
Tim <81175852+wharvex@users.noreply.github.com>
2025-01-25 14:36:37 -0500

    first commit

---
+++ OracleTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace OracleTest;
+
+internal class Program
+{
+    public static string user = "DEMODOTNET";
+    public static string pwd = "PASSWORD";
+    public static string db = "localhost/XEPDB1";
+
+    static void Main(string[] args)
+    {
+        string conStringUser = "User Id=" + user + ";Password=" + pwd + ";Data Source=" + db + ";";
+
+        using OracleConnection con = new OracleConnection(conStringUser);
+        using OracleCommand cmd = con.CreateCommand();
+        try
+        {
+            con.Open();
+            Console.WriteLine("Successfully connected to Oracle Database as " + user);
+            Console.WriteLine();
+
+            //Retrieve sample data
+            cmd.CommandText = "SELECT description, done FROM todoitem";
+            OracleDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.GetBoolean(1))
+                    Console.WriteLine(reader.GetString(0) + " is done.");
+                else
+                    Console.WriteLine(reader.GetString(0) + " is NOT done.");
+            }
+
+            reader.Dispose();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
